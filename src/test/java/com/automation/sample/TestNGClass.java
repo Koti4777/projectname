355ce41d159f1d0a76b5c94805dbf8a5215e8934
Koteswararao Majji <koteswararao.majji@volantetech.net>
2022-07-22 15:31:24 +0530

    first project for triggering execution form maven using Jenkins

---
+++ src/test/java/com/automation/sample/TestNGClass.java
@@ -0,0 +1,36 @@
+package com.automation.sample;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+
+public class TestNGClass {
+
+	 @Test
+	    public void shouldAnswerWithTrue()
+	    {
+	      Reporter.log("test case one");
+	    }
+	 
+	 @Test
+	    public void shouldAnswerWithTrue1()
+	    {
+		 Reporter.log("test case two");
+		  String str2="two";
+	       Assert.assertEquals("two", str2,"testng testing");
+	      
+	    }
+	 
+	 @Test
+	    public void shouldAnswerWithTrue2()
+	    {
+		 Reporter.log("Test case three jfhjfkdj ");
+	       String str2="two";
+	       Assert.assertEquals("two", str2);
+	       Reporter.log("Test case three");
+	       Reporter.log("\n"+"Test case next line");
+	       
+	    }
+}
